f9cb2978830427c32f743d4e408ddc7f4eadcc06
nv-vn <nv@cock.li>
2015-06-25 18:21:33 -0400

    Added code to handle nested subexpressions... only works left->right

--- src/interpreter.ml
+++ src/interpreter.ml
@@ -1,19 +1,23 @@
 open Ast
 
-let prefix_plus = function
+let rec prefix_plus = function
   | List l -> List l
+  | Operation o -> interpret (Operation o) |> prefix_plus
   | _ -> failwith "Invalid application of prefix function" |> (fun _ -> List [])
-let prefix_minus = function
+and prefix_minus = function
   | List l -> List (List.map (fun n -> -.n) l)
+  | Operation o -> interpret (Operation o) |> prefix_minus
   | _ -> failwith "Invalid application of prefix function" |> (fun _ -> List [])
-let prefix_times = function
+and prefix_times = function
   | List l -> List (List.map (fun n -> if n > 0.0 then 1.0 else if n < 0.0 then -1.0 else 0.0) l)
+  | Operation o -> interpret (Operation o) |> prefix_times
   | _ -> failwith "Invalid application of prefix function" |> (fun _ -> List [])
-let prefix_divide = function
+and prefix_divide = function
   | List l -> List (List.map (fun n -> 1.0 /. n) l)
+  | Operation o -> interpret (Operation o) |> prefix_divide
   | _ -> failwith "Invalid application of prefix function" |> (fun _ -> List [])
 
-let infix_plus a b =
+and infix_plus a b =
   match (a, b) with
   | (List a', List b') ->
     begin
@@ -22,8 +26,13 @@ let infix_plus a b =
       else
         List (List.map2 (fun l r -> l +. r) a' b')
     end
+  | (List _, Operation _) ->
+    begin
+      let r = interpret b in
+      infix_plus a r
+    end
   | _ -> failwith "Invalid application of infix function" |> (fun _ -> List [])
-let infix_minus a b =
+and infix_minus a b =
   match (a, b) with
   | (List a', List b') ->
     begin
@@ -32,8 +41,13 @@ let infix_minus a b =
       else
         List (List.map2 (fun l r -> l -. r) a' b')
     end
+  | (List _, Operation _) ->
+    begin
+      let r = interpret b in
+      infix_minus a r
+    end
   | _ -> failwith "Invalid application of infix function" |> (fun _ -> List [])
-let infix_times a b =
+and infix_times a b =
   match (a, b) with
   | (List a', List b') ->
     begin
@@ -42,8 +56,13 @@ let infix_times a b =
       else
         List (List.map2 (fun l r -> l *. r) a' b')
     end
+  | (List _, Operation _) ->
+    begin
+      let r = interpret b in
+      infix_times a r
+    end
   | _ -> failwith "Invalid application of infix function" |> (fun _ -> List [])
-let infix_divide a b =
+and infix_divide a b =
   match (a, b) with
   | (List a', List b') ->
     begin
@@ -52,8 +71,13 @@ let infix_divide a b =
       else
         List (List.map2 (fun l r -> l /. r) a' b')
     end
+  | (List _, Operation _) ->
+    begin
+      let r = interpret b in
+      infix_divide a r
+    end
   | _ -> failwith "Invalid application of infix function" |> (fun _ -> List [])
-let infix_reduce a b =
+and infix_reduce a b =
   match (a, b) with
   | (List a', List b') ->
     begin
@@ -62,9 +86,14 @@ let infix_reduce a b =
       else
         List (Util.multimap2 (fun l r -> if l < 0.0 then Batteries.List.make (-int_of_float l) 0.0 else BatList.make (int_of_float l) r) a' b')
     end
+  | (List _, Operation _) ->
+    begin
+      let r = interpret b in
+      infix_reduce a r
+    end
   | _ -> failwith "Invalid application of infix function" |> (fun _ -> List [])
 
-let interpret = function
+and interpret = function
   | List l -> List l
   | Operation a ->
     begin
